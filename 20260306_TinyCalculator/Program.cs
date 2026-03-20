5993b07476748c707447c5faa2ff71208b6db68f
Hofmaninger Sebastian <S.Hofmaninger@htlvb.at>
2026-03-20 09:07:45 +0100

    Show colored operands

--- 20260306_TinyCalculator/Program.cs
+++ 20260306_TinyCalculator/Program.cs
@@ -2,9 +2,13 @@
 Console.WriteLine("== Tiny Calculator ==");
 
 Console.Write("Enter Operand1: ");
+Console.ForegroundColor = ConsoleColor.Red;
 double a = double.Parse(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.White;
 Console.Write("Enter Operand2: ");
+Console.ForegroundColor = ConsoleColor.Red;
 double b = double.Parse(Console.ReadLine());
+Console.ForegroundColor = ConsoleColor.White;
 Console.WriteLine($"{a} + {b} = {a + b}");
 Console.WriteLine($"{a} - {b} = {a - b}");
 Console.WriteLine($"{a} * {b} = {a * b}");
