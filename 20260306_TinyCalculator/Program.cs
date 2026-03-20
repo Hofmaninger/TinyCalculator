ec21ce7a29190ad7bf16ff6271fc70feddd7120c
Hofmaninger Sebastian <S.Hofmaninger@htlvb.at>
2026-03-20 08:39:09 +0100

    Show calculation result from all basic operators

--- 20260306_TinyCalculator/Program.cs
+++ 20260306_TinyCalculator/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("== Tiny Calculator ==");
+
+Console.Write("Enter Operand1: ");
+double a = double.Parse(Console.ReadLine());
+Console.Write("Enter Operand2: ");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine($"{a} + {b} = {a + b}");
+Console.WriteLine($"{a} - {b} = {a - b}");
+Console.WriteLine($"{a} * {b} = {a * b}");
+Console.WriteLine($"{a} / {b} = {a / b}");
